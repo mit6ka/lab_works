6e0f4912361a7155c83c92cb4d785f39f5270f23
mit6ka <151203814+mit6ka@users.noreply.github.com>
2024-03-15 21:59:52 +0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,47 +1,47 @@
 #Вариант 10.
 #Натуральные числа. Выводить, меняя порядок цифр в них на обратный.
-#Для чисел, состоящих из одинаковых цифр вывод должен иметь вид – « 120 единиц».
-def reverse_number(n):
-    return n[::-1]
-def is_uniform(n):
-    return all(digit == n[0] for digit in n)
-def process_lexeme(lexeme):
-    if not lexeme.isalnum():
-        return
-    if lexeme.isdigit():
-        if is_uniform(lexeme):
-            digit_word = {
-                '0': 'нулей',
-                '1': 'единиц',
-                '2': 'двоек',
-                '3': 'троек',
-                '4': 'четверок',
-                '5': 'пятерок',
-                '6': 'шестерок',
-                '7': 'семерок',
-                '8': 'восьмерок',
-                '9': 'девяток'
-            }
-            digit = lexeme[0]
-            print(f"{len(lexeme)} {digit_word.get(digit, 'единиц')}")
+#Для чисел, состоящих из одинаковых цифр вывод должен иметь вид – « 120 единиц».'''
+
+def reverse_digits(number):
+
+    return int(str(number)[::-1])
+
+def number_to_words(len_number, number):
+   
+    words_dict = {
+        '0': 'ноль',
+        '1': 'единиц',
+        '2': 'двоек',
+        '3': 'троек',
+        '4': 'четверок',
+        '5': 'пятерок',
+        '6': 'шестерок',
+        '7': 'семерок',
+        '8': 'восемерок',
+        '9': 'девяток'
+    }
+    return words_dict.get(str(number), str(len_number))
+
+def process_sequence(sequence):
+  
+    lexemes = sequence.split() 
+    for lexeme in lexemes:
+        if lexeme.isdigit():
+            num = int(lexeme)
+            reversed_num = reverse_digits(num)
+            if reversed_num == num:
+                print(f"{len(lexeme)} {number_to_words(len(lexeme), lexeme[0])}")
+            else:
+                print(reverse_digits(num))
         else:
-            print(reverse_number(lexeme))
-    else:
-        if not any(char.isdigit() for char in lexeme):
-            return
-        print(lexeme)
-def read_and_process_file(filename):
-    lexeme_buffer = ''
-    with open(filename, 'r') as f:
-        while True:
-            block = f.read(1024)
-            if not block:
-                break
-            lexeme_buffer += block
-            words = lexeme_buffer.split(' ')
-            lexeme_buffer = words.pop()
-            for word in words:
-                process_lexeme(word)
-    if lexeme_buffer:
-        process_lexeme(lexeme_buffer)
-read_and_process_file('inputs.txt')
+            pass
+try:
+    with open("input.txt", "r") as file:
+        data = file.read()
+        if data:
+            filtered_data = ''.join(filter(lambda x: x.isdigit() or x.isspace(), data))
+            process_sequence(filtered_data)
+        else:
+            print('Файл пуст')
+except FileNotFoundError:
+    print("Файл input.txt не найден.")
